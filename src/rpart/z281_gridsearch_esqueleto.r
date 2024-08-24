9383a1a5343c22cc4a2a39b9401407ad433622c9
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-08-24 06:26:16 -0300

    z281_gridsearch_esqueleto.r

--- src/rpart/z281_gridsearch_esqueleto.r
+++ src/rpart/z281_gridsearch_esqueleto.r
@@ -18,8 +18,8 @@ PARAM$qsemillas <- 20
 PARAM$training_pct <- 70L  # entre  1L y 99L 
 
 # elegir SU dataset comentando/ descomentando
-PARAM$dataset_nom <- "./datasets/vivencial_dataset_pequeno.csv"
-# PARAM$dataset_nom <- "./datasets/conceptual_dataset_pequeno.csv"
+PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
+# PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
 
 #------------------------------------------------------------------------------
 # particionar agrega una columna llamada fold a un dataset
