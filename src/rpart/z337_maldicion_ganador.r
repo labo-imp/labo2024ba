48c6c4b55cce9f97f15ef877cdca43629e5d1e66
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-08-27 08:26:52 -0300

    z337_maldicion_ganador.r

---
+++ src/rpart/z337_maldicion_ganador.r
@@ -0,0 +1,131 @@
+rm(list = ls()) # Borro todos los objetos
+gc() # Garbage Collection
+
+require("data.table")
+require("rpart")
+require("parallel")
+require("primes")
+
+
+PARAM <- list()
+# reemplazar por las propias semillas
+PARAM$qsemillas <- 200
+
+
+PARAM$training_pct <- 70L  # entre  1L y 99L 
+
+PARAM$rpart <- list (
+  "cp" = -1,
+  "minsplit" = 800,
+  "minbucket" = 200,
+  "maxdepth" = 6
+)
+
+#------------------------------------------------------------------------------
+# particionar agrega una columna llamada fold a un dataset que consiste
+#  en una particion estratificada segun agrupa
+# particionar( data=dataset, division=c(70,30), agrupa=clase_ternaria, seed=semilla)
+#  crea una particion 70, 30
+
+particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, seed = NA) {
+  if (!is.na(seed)) set.seed(seed)
+
+  bloque <- unlist(mapply(function(x, y) {
+    rep(y, x)
+  }, division, seq(from = start, length.out = length(division))))
+
+  data[, (campo) := sample(rep(bloque, ceiling(.N / length(bloque))))[1:.N],
+    by = agrupa
+  ]
+}
+#------------------------------------------------------------------------------
+
+ArbolEstimarGanancia <- function(semilla, param_basicos) {
+  # particiono estratificadamente el dataset
+  particionar(dataset,
+    division = c(param_basicos$training_pct, 100L -param_basicos$training_pct), 
+    agrupa = "clase_ternaria",
+    seed = semilla # aqui se usa SU semilla
+  )
+
+  # genero el modelo
+  # predecir clase_ternaria a partir del resto
+  modelo <- rpart("clase_ternaria ~ .",
+    data = dataset[fold == 1], # fold==1  es training,  el 70% de los datos
+    xval = 0,
+    control = param_basicos$rpart
+  ) # aqui van los parametros del arbol
+
+  # aplico el modelo a los datos de testing
+  prediccion <- predict(modelo, # el modelo que genere recien
+    dataset[fold == 2], # fold==2  es testing, el 30% de los datos
+    type = "prob"
+  ) # type= "prob"  es que devuelva la probabilidad
+
+  # prediccion es una matriz con TRES columnas,
+  #  llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
+  # cada columna es el vector de probabilidades
+
+
+  # calculo la ganancia en testing  qu es fold==2
+  ganancia_test <- dataset[
+    fold == 2,
+    sum(ifelse(prediccion[, "BAJA+2"] > 0.025,
+      ifelse(clase_ternaria == "BAJA+2", 117000, -3000),
+      0
+    ))
+  ]
+
+  # escalo la ganancia como si fuera todo el dataset
+  ganancia_test_normalizada <- ganancia_test / (( 100 - PARAM$training_pct ) / 100 )
+
+  return(list(
+    "semilla" = semilla,
+    "testing" = dataset[fold == 2, .N],
+    "testing_pos" = dataset[fold == 2 & clase_ternaria == "BAJA+2", .N],
+    "envios" = dataset[fold == 2, sum(prediccion[, "BAJA+2"] > 0.025)],
+    "aciertos" = dataset[
+        fold == 2,
+        sum(prediccion[, "BAJA+2"] > 0.025 & clase_ternaria == "BAJA+2")
+    ],
+    "ganancia_test" = ganancia_test_normalizada
+  ))
+}
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+setwd("~/buckets/b1/") # Establezco el Working Directory
+
+#cargo miAmbiente
+miAmbiente <- read_yaml( "~/buckets/b1/miAmbiente.yml" )
+
+
+# genero numeros primos
+primos <- generate_primes(min = 100000, max = 1000000)
+set.seed(miAmbiente$semilla_primigenia) # inicializo 
+# me quedo con PARAM$qsemillas   semillas
+PARAM$semillas <- sample(primos, PARAM$qsemillas )
+
+
+# cargo dataset
+dataset <- fread( miAmbiente$dataset_pequeno )
+
+# trabajo solo con los datos con clase, es decir 202107
+dataset <- dataset[clase_ternaria != ""]
+
+
+# la funcion mcmapply  llama a la funcion ArbolEstimarGanancia
+#  tantas veces como valores tenga el vector  PARAM$semillas
+salidas <- mcmapply(ArbolEstimarGanancia,
+  PARAM$semillas, # paso el vector de semillas
+  MoreArgs = list(PARAM), # aqui paso el segundo parametro
+  SIMPLIFY = FALSE,
+  mc.cores = detectCores()
+)
+
+
+# paso la lista a vector
+tb_salida <- rbindlist(salidas)
+
+cat( "ganancia media :", tb_salida[, mean(ganancia_test)], "\n" )
+
